4642c595fadc80d42aad204565b33c1de440b996
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2022-04-27 11:06:29 -0300

    commit 01

---
+++ 01.js
@@ -0,0 +1,6 @@
+ const nome = prompt('Qual é o seu nome ')
+alert(`Seja bem vindo ${nome}, esse site foi criado pelo Caio Mafra, utilizando HTML é CSS , para observar outros projetos acesse o GitHub ou LinkedIn `)
+
+const usu = `Continuar como ${nome}`;
+
+document.querySelector('#init').innerHTML = usu
---
+++ README.md
@@ -0,0 +1,2 @@
+# matrix
+ proeto focado no matrix 
