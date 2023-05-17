8afd4cfb5bc8f2cc5f332415756b00f0c6f7c1c9
Caio Mafra <gamersb200@gmail.com>
2023-05-17 20:17:54 -0300

    Atualização

--- assents/js/script.js
+++ assents/js/script.js
@@ -1,10 +1,11 @@
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
+
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "assets/img/menu-aberto.png";
+        document.querySelector('.icon').src = "assents/img/closemenu.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "assets/img/closemenu.png";
+        document.querySelector('.icon').src = "assents/img/menu-aberto.png";
     }
 }
