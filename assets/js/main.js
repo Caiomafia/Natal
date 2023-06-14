4a11824da39cab110e502e550404edb453f95a5f
Caio Mafra <gamersb200@gmail.com>
2023-06-13 21:59:05 -0300

    Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,59 +82,6 @@ sr.reveal(`.logos__img, .program__card, .pricing__card`, { interval: 100 })
 sr.reveal(`.choose__img, .calculate__content`, { origin: 'left' })
 sr.reveal(`.choose__content, .calculate__img`, { origin: 'right' })
 
-/*=============== CALCULATE JS ===============*/
-const calculateForm = document.getElementById('calculate-form'),
-    calculateCm = document.getElementById('calculate-cm'),
-    calculateKg = document.getElementById('calculate-kg'),
-    calculateMessage = document.getElementById('calculate-message')
-
-const calculateBmi = (e) => {
-    e.preventDefault()
-
-    // Check if the fields have a value
-    if (calculateCm.value === '' || calculateKg.value === '') {
-        // Add and remove color
-        calculateMessage.classList.remove('color-green')
-        calculateMessage.classList.add('color-red')
-
-        // Show message
-        calculateMessage.textContent = 'Fill in the Height and Weight 👨‍💻'
-
-        // Remove message three seconds
-        setTimeout(() => {
-            calculateMessage.textContent = ''
-        }, 3000)
-    } else {
-        // BMI Formula
-        const cm = calculateCm.value / 100,
-            kg = calculateKg.value,
-            bmi = Math.round(kg / (cm * cm))
-
-        // Show your health status
-        if (bmi < 18.5) {
-            // Add color and display message
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are skinny 😔`
-        } else if (bmi < 25) {
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are healthy 🥳`
-        } else {
-            calculateMessage.classList.add('color-green')
-            calculateMessage.textContent = `Your BMI is ${bmi} and you are overweight 😔`
-        }
-
-        // To clear the input field
-        calculateCm.value = ''
-        calculateKg.value = ''
-
-        // Remove message four seconds
-        setTimeout(() => {
-            calculateMessage.textContent = ''
-        }, 4000)
-    }
-}
-
-calculateForm.addEventListener('submit', calculateBmi)
 
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
