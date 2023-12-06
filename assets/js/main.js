3f442164897ec3ec6e6c9ddae051021f17e35eba
Caio Mafra <91575842+Caiomafia@users.noreply.github.com>
2023-12-06 18:39:28 -0300

    Commit init

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,35 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+             navToggle = document.getElementById('nav-toggle'),
+             navClose = document.getElementById('nav-close')
+
+
+if(navToggle){
+    navToggle.addEventListener('click' , () => {
+        navMenu.classList.add('active-menu')
+    })
+}
+
+
+if(close){
+    navClose.addEventListener('click' , () => {
+        navMenu.classList.remove('active-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+
+const navlink = document.querySelectorAll('.nav_link')
+
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('active-menu')
+}
+
+navlink.forEach(n=>n.addEventListener('click' , linkAction))
 
 
 /*=============== DAY COUNTER FOR CHRISTMAS ===============*/
